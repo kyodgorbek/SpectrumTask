61841d2613fd1707393eb7b7f1425530d150b429
kyodgorbek@gmail.com <adilnoz1992>
2020-08-15 17:57:42 +0200

    fixed crashes

--- app/src/main/java/yodgorbekkomilov/edgar/spectrumtask/LauncherActivity.kt
+++ app/src/main/java/yodgorbekkomilov/edgar/spectrumtask/LauncherActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_launcher.*
 
+
 class LauncherActivity :AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
