3ee9b7b0817bbc51f4542230a1155fb16449730a
kyodgorbek@gmail.com <adilnoz1992>
2020-08-17 11:51:54 +0200

    added searchview to MemberActivity.kt

--- app/src/main/java/yodgorbekkomilov/edgar/spectrumtask/MemberActivity.kt
+++ app/src/main/java/yodgorbekkomilov/edgar/spectrumtask/MemberActivity.kt
@@ -3,6 +3,7 @@ package yodgorbekkomilov.edgar.spectrumtask
 import android.os.Bundle
 import android.view.View
 import android.widget.LinearLayout
+import android.widget.SearchView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -11,36 +12,63 @@ import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.progressBar
+import kotlinx.android.synthetic.main.activity_main.recyclerView
+import kotlinx.android.synthetic.main.activity_main.searchView
+import kotlinx.android.synthetic.main.activity_member.*
 import yodgorbekkomilov.edgar.spectrumtask.adapter.MemberAdapter
 import yodgorbekkomilov.edgar.spectrumtask.network.ServiceBuilder
 import yodgorbekkomilov.edgar.spectrumtask.network.SpectrumInterface
 
-class MemberActivity : AppCompatActivity() {
 
+class MemberActivity : AppCompatActivity() {
+    private val compositeDisposable by lazy { CompositeDisposable() }
+    private val buildService by lazy { ServiceBuilder.buildService(SpectrumInterface::class.java) }
+    private var memberAdapter: MemberAdapter? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_member)
 
-        val compositeDisposable = CompositeDisposable()
-        compositeDisposable.add(
-            ServiceBuilder.buildService(SpectrumInterface::class.java)
-                .getMembers()
-                .toObservable()
-                .flatMap { Observable.fromIterable(it) }
-                .flatMap { Observable.fromIterable(it.members) }
-                .toList()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                    { members -> onResponse(members) },
-                    { t -> onFailure(t) })
-        )
+
+        compositeDisposable.add(buildService
+            .getMembers()
+            .toObservable()
+            .flatMap { Observable.fromIterable(it) }
+            .flatMap { Observable.fromIterable(it.members) }
+            .toList()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { if (compositeDisposable.isDisposed.not()) onResponse(it) },
+                { onFailure(it) }
+            ))
 
         recyclerView.adapter = MemberAdapter()
         recyclerView.setHasFixedSize(true)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.addItemDecoration(DividerItemDecoration(this, LinearLayout.VERTICAL))
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                memberAdapter?.getFilter()?.filter(newText)
+
+                return true
+            }
+
+        })
+        ascendingButton.setOnClickListener {
+            memberAdapter?.sortAscending()
+        }
+
+        descendingButton.setOnClickListener {
+            memberAdapter?.sortDescending()
+        }
+
+
     }
 
     private fun onFailure(t: Throwable) {
@@ -51,4 +79,28 @@ class MemberActivity : AppCompatActivity() {
         progressBar.visibility = View.GONE
         (recyclerView.adapter as MemberAdapter).setMembers(members)
     }
+
+    override fun onPause() {
+        super.onPause()
+        compositeDisposable.dispose()
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/yodgorbekkomilov/edgar/spectrumtask/adapter/MemberAdapter.kt
+++ app/src/main/java/yodgorbekkomilov/edgar/spectrumtask/adapter/MemberAdapter.kt
@@ -3,29 +3,82 @@ package yodgorbekkomilov.edgar.spectrumtask.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import yodgorbekkomilov.edgar.spectrumtask.Member
 import yodgorbekkomilov.edgar.spectrumtask.R
+import yodgorbekkomilov.edgar.spectrumtask.SpectrumResponseItem
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MemberAdapter : RecyclerView.Adapter<MemberAdapter.MemberViewHolder>() {
-    private val members = mutableListOf<Member>()
+    private var members = mutableListOf<Member>()
+    private val filteredMembers = mutableListOf<Member>()
 
     fun setMembers(data: List<Member>) {
         members.clear()
         members.addAll(data)
+        members = filteredMembers
         notifyDataSetChanged()
     }
+
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MemberViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.member_list, parent, false)
         return MemberViewHolder(view)
     }
 
-    override fun getItemCount() = members.size
+    override fun getItemCount() = filteredMembers.size
 
+
+
+
+    fun sortAscending() {
+        filteredMembers.sortBy { it.age }
+        notifyDataSetChanged()
+    }
+
+    fun sortDescending() {
+        filteredMembers.sortByDescending { it.age }
+        notifyDataSetChanged()
+    }
+
+    fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(charSequence: CharSequence): FilterResults {
+                val query = charSequence.toString()
+
+                val filtered: MutableList<Member> = ArrayList()
+
+                if (query.isEmpty()) {
+                    filtered.clear()
+                    filtered.addAll(filteredMembers)
+                } else {
+                    filtered.addAll(filteredMembers.filter {
+                        it.id.toLowerCase(Locale.ROOT)
+                            .contains(query.toLowerCase(Locale.ROOT)) || it.age.toString()
+                            .contains(query)
+                    })
+                }
+
+                val results = FilterResults()
+                results.count = filtered.size
+                results.values = filtered
+                return results
+            }
+
+            override fun publishResults(charSequence: CharSequence, results: FilterResults) {
+                filteredMembers.clear()
+                filteredMembers.addAll(results.values as Collection<Member>)
+
+                notifyDataSetChanged()
+            }
+        }
+    }
     override fun onBindViewHolder(holder: MemberViewHolder, position: Int) {
-        return holder.bind(members[position])
+        return holder.bind(filteredMembers[position])
     }
 
     class MemberViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
